512d3f8c2ec2bd05bb745d01d74702304ccdf5dc
swoo <kokodd1234@gmail.com>
2026-02-26 15:10:18 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/main/java/org/example/be24swooobackend/Be24SwoooBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.be24swooobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be24SwoooBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Be24SwoooBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/be24swooobackend/Be24SwoooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.be24swooobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be24SwoooBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
